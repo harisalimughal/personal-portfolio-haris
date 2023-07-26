827f6543f733bc789ac3c50c9e6bb241f761c46a
Haris <119020619+harisalimughal@users.noreply.github.com>
2023-07-26 15:28:08 +0500

    Initial commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [ setIndex] = useState(1);
+  const [, setIndex] = useState(1);
   const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
   const period = 2000;
 
